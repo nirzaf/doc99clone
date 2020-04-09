29f4410fc9793a741ef63e03c093d5a2ffe0613b
Mohamed Fazrin <mohamed.fazrin@staff.broadbandsolutions.com.au>
2020-04-10 02:45:11 +0530

    recent changes

--- docapi/serializers.py
+++ docapi/serializers.py
@@ -1,7 +1,23 @@
+from abc import ABC
+
 from rest_framework import serializers
 from .models import Users
 from .models import Doctors
 from .models import Hospitals
 from .models import HospitalStaff
 from .models import Booking
-from .models import Ses
+from .models import Session
+
+
+class UsersSerializers(serializers.Serializer):
+    Id = serializers.BigIntegerField(primary_key=True)
+    First_Name = serializers.CharField(max_length=100)
+    Last_Name = serializers.CharField(max_length=100)
+    Username = serializers.CharField(max_length=50)
+    Password = serializers.CharField(max_length=50)
+    Mobile_No = serializers.CharField(max_length=20)
+    Auth_Token = serializers.CharField(max_length=200)
+    Is_Deleted = serializers.BooleanField(default=True)
+
+    def create(self, validated_data):
+        return Users.objects.create(validated_data)
