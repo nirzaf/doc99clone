38f53a25017a48413723612ece8dfcf0f072e4de
Mohamed Fazrin <mohamed.fazrin@staff.broadbandsolutions.com.au>
2020-04-10 00:52:04 +0530

    project init

--- docapi/models.py
+++ docapi/models.py
@@ -5,12 +5,12 @@ from django.db import models
 class Users(models.Model):
     Id = models.BigIntegerField(primary_key=True)
     First_Name = models.CharField(max_length=100)
-    Last_Name = models.CharField(max_lenght=100)
-    Username = models.CharField(max_legnth=50)
+    Last_Name = models.CharField(max_length=100)
+    Username = models.CharField(max_length=50)
     Password = models.CharField(max_length=50)
     Mobile_No = models.CharField(max_length=20)
     Auth_Token = models.CharField(max_length=200)
-    Is_Deleted = models.BooleanField(initial=True)
+    Is_Deleted = models.BooleanField(default=True)
 
     def __str__(self):
         return self.Id
