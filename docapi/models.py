bb5c847823e0db1afc034f4dbc47d0fb8c166514
Mohamed Fazrin <mohamed.fazrin@staff.broadbandsolutions.com.au>
2020-04-10 02:34:08 +0530

    recent changes

--- docapi/models.py
+++ docapi/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from datetime import datetime
 
 
 # Create your models here.
@@ -14,3 +15,68 @@ class Users(models.Model):
 
     def __str__(self):
         return self.Id
+
+
+class Doctors(models.Model):
+    Id: int = models.BigIntegerField(primary_key=True)
+    First_Name = models.CharField(max_length=100)
+    Last_Name = models.CharField(max_length=100)
+    Mobile_No = models.CharField(max_length=20)
+    Category = models.CharField(max_length=200)
+    Is_Deleted = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.Id
+
+
+class Hospitals(models.Model):
+    Id: int = models.BigIntegerField(primary_key=True)
+    Name = models.CharField(max_length=200)
+    Address = models.TextField(max_length=256)
+    Telephone = models.CharField(max_length=20)
+    Email = models.EmailField(max_length=100)
+    Is_Deleted = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.Id
+
+
+class HospitalStaff(models.Model):
+    Id: int = models.BigIntegerField(primary_key=True)
+    Username = models.CharField(max_length=100)
+    Password = models.CharField(max_length=100)
+    First_Name = models.CharField(max_length=100)
+    Last_Name = models.CharField(max_length=100)
+    Address = models.TextField(max_length=256)
+    StaffId = models.CharField(max_length=50)
+    User_Type = models.IntegerField(default=0)
+    Hospital_Id = models.ForeignKey(Hospitals, on_delete=models.CASCADE)
+    Is_Deleted = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.Id
+
+
+class Session(models.Model):
+    Session_Id: str = models.CharField(max_length=100, primary_key=True)
+    Date = models.DateField(default=datetime.now)
+    Slot_Begin = models.CharField(max_length=10)
+    Slot_End = models.CharField(max_length=10)
+    Max_Limit = models.IntegerField(default=0)
+    Is_Deleted = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.Session_Id
+
+
+class Booking(models.Model):
+    Booking_Id = models.BigIntegerField(primary_key=True)
+    Booked_By = models.IntegerField(default=0)
+    User_Id = models.ForeignKey(Users, on_delete=models.CASCADE)
+    Session_Id = models.ForeignKey(Session, models.CASCADE)
+    Is_Paid = models.BooleanField(default=False)
+    Is_Canceled = models.BooleanField(default=False)
+    Is_Deleted = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.Booking_Id
---
+++ docapi/serializers.py
@@ -0,0 +1,7 @@
+from rest_framework import serializers
+from .models import Users
+from .models import Doctors
+from .models import Hospitals
+from .models import HospitalStaff
+from .models import Booking
+from .models import Ses
