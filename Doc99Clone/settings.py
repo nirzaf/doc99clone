cb192dc9d097ac6f3fa3f51f52c4ae8d828f8485
razmy <razmyasdf@gmail.com>
2020-04-15 01:25:03 +0530

    razmy 'ubuntu changes'

--- Doc99Clone/settings.py
+++ Doc99Clone/settings.py
@@ -80,7 +80,7 @@ DATABASES = {
         'NAME': 'django',
         'USER': 'root',
         'PASSWORD': '',
-        'HOST': 'localhost',
+        'HOST': '127.0.0.1',
         'PORT': '3306'
     }
 }
